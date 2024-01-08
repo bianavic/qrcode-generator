71af7f2188f5ffa58ea8407ecce485d1d3cb90e6
bianavic <bianavic@gmail.com>
2024-01-08 16:49:45 -0300

    initial commit

---
+++ src/main/java/org/edu/fabs/qrcodegenerator/QrcodeGeneratorApplication.java
@@ -0,0 +1,13 @@
+package org.edu.fabs.qrcodegenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrcodeGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrcodeGeneratorApplication.class, args);
+	}
+
+}
