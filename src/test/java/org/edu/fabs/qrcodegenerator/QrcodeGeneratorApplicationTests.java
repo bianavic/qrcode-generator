26bc43855eb611d13b33746c8eb39792a521aa5e
bianavic <bianavic@gmail.com>
2024-01-11 18:20:22 -0300

    test(service): implement tests for qrcode

--- src/test/java/org/edu/fabs/qrcodegenerator/QrcodeGeneratorApplicationTests.java
+++ src/test/java/org/edu/fabs/qrcodegenerator/QrcodeGeneratorApplicationTests.java
@@ -1,13 +1,13 @@
-//package org.edu.fabs.qrcodegenerator;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class QrcodeGeneratorApplicationTests {
-//
-//    @Test
-//    void contextLoads() {
-//    }
-//
-//}
+package org.edu.fabs.qrcodegenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrcodeGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
